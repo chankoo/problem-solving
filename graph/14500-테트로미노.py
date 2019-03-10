dfac75e8f6842db6882bf8dabaae733cabac11d4
chankoo <ckbaek1125@gmail.com>
2019-03-10 17:30:42 +0900

    19.03.10

--- graph/14500-테트로미노.py
+++ graph/14500-테트로미노.py
@@ -1,13 +1,14 @@
 # https://www.acmicpc.net/problem/14500
-   
 
-# nums의 방문 여부를 표시한 리스트 visited를 reset하는 함수
-## -> 사용하지 말자(시간초과)
-# def reset_visited():    
-#     visited =[]
-#     for _ in range(N):
-#         visited.append([False for _ in range(M)])
-#     return visited
+# solution
+# 1) 초기 좌표 (i,j)를 정한다   
+# 2) 인접한 좌표에 대해 dfs 하며 4개 블럭으로 가능한 합의 최대값을 갱신한다
+# 3) dfs 불가능한 'ㅗ' 모양 블럭으로 가능한 값을 계산해 최대값을 갱신한다
+# 4) 1)로 돌아가 새로운 초기 좌표(i, j) 정한다. brute-forcely 순회한다
+# 5) 순회를 마치고 최대값을 출력한다
+
+# TIL
+# O(N)인 초기화 함수(reset_visited)를 brute-force에 이용하는 것은 가볍지 않은 코드이다. 결과적으로 O(N^2)가 된다
 
 
 # 테트로미노의 길이인 4 level 동안 현재 노드에서 가능한 방향을 모두 탐색하는 dfs 함수
@@ -61,6 +62,14 @@ def fucking_block(i,j): # 'ㅗ' 모양 블럭으로 가능한 결과 계산하�
             max_sum = max(max_sum, sum_directly(locs))
 
 
+# nums의 방문 여부를 표시한 리스트 visited를 reset하는 함수
+## -> 사용하지 말자(시간초과)
+# def reset_visited():    
+#     visited =[]
+#     for _ in range(N):
+#         visited.append([False for _ in range(M)])
+#     return visited
+
 if __name__ == "__main__":
     N,M = tuple(map(int, input().split()))
 
@@ -76,7 +85,6 @@ if __name__ == "__main__":
     for i in range(N):
         for j in range(M):
             # visited = reset_visited() # -> 사용하지 말자
-            ## 조심!! reset_visited를 이용해 매번 초기화하는 코드로는 시간초과 뜬다
 
             visited[i][j] = True # (i,j)를 방문한 상태
             dfs(i,j,1,nums[i][j]) # (i,j)에서부터 dfs 탐색
---
+++ graph/14502-연구소.py
@@ -0,0 +1,84 @@
+# https://www.acmicpc.net/problem/14502
+
+# solution
+
+# 1) 임의로 3개의 벽을 세운다(dfs)
+# 2) 바이러스를 퍼뜨린다(dfs)
+# 3) 안전구역의 크기를 구해서 최대값을 갱신한다
+# 4) 1)로 돌아가 모든 경우에 대해 반복한다
+# 5) 안전구역 크기의 최대값을 출력한다
+
+# TIL
+# 2차원 배열에서 가능한 모든 조합을 재귀적으로 탐색하는 코드( line: )
+## -> 몫(i//m)과 나머지(i%m)를 이용한 탐색과 재귀함수 내에 반복문 있는 형태 
+
+import copy
+from collections import deque
+
+
+def calc_safe_area():
+    global walled_map
+    
+    area = 0
+    for i in range(N):
+        for j in range(M):
+            if walled_map[i][j] == 0:
+                area += 1
+    return area
+
+
+def virus_spread(virus_lst_copy): # 초기의 virus_lst를 받아서 bfs로 virus 퍼뜨림
+    global walled_map
+    dx = [1,0,-1,0]
+    dy = [0,1,0,-1]
+
+    q = deque(virus_lst_copy)
+    while len(q) > 0:
+        x, y = q.popleft()
+        for d_idx in range(4):
+            next_x = x + dx[d_idx]
+            next_y = y + dy[d_idx]
+
+            if ( (next_x >=0) and (next_x <N) and (next_y >=0) and (next_y <M) and (walled_map[next_x][next_y]==0) ):
+                walled_map[next_x][next_y] = 2 
+                q.append((next_x,next_y))
+
+
+def calculate(start, wall_cnt): # 벽을 세운 뒤 바이러스 퍼뜨리고 안전구역 넓이 계산해 최대값일 경우 갱신
+    global lab_map, max_area, N, M
+
+    if wall_cnt == 3: # 3개의 벽 모두 세워짐
+        global walled_map
+        walled_map = copy.deepcopy(lab_map)
+        virus_lst_copy = copy.deepcopy(virus_lst)
+
+        virus_spread(virus_lst_copy)
+
+        max_area = max(max_area, calc_safe_area())
+        return
+
+    for i in range(start, N*M):
+        x = i//M
+        y = i%M
+        if lab_map[x][y] == 0:
+            lab_map[x][y] = 1
+            calculate(i+1,wall_cnt+1)
+            lab_map[x][y] = 0
+
+
+if __name__ == "__main__":
+    N, M = tuple(map(int, input().split()))
+
+    lab_map = []
+    for _ in range(N):
+        lab_map.append(list(map(int, input().split())))
+
+    virus_lst = []
+    for i in range(N):
+        for j in range(M):
+            if lab_map[i][j] == 2:
+                 virus_lst.append((i,j))
+
+    max_area = 0 # 안전구역의 최대크기
+    calculate(0,0)
+    print(max_area)
